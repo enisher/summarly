92595db4ab06134b878e1fc015ab4d556243d951
pesua <pesua.mail@gmail.com>
2014-09-13 17:47:56 +0300

    Introduced rank modifiers

--- summarization-lib/src/main/java/org/summarly/LexRankSummarizationService.java
+++ summarization-lib/src/main/java/org/summarly/LexRankSummarizationService.java
@@ -6,8 +6,11 @@ import org.summarly.segmentation.StanfordNLPSplitter;
 import org.summarly.segmentation.TextSplitter;
 import org.summarly.summarizing.Filter;
 import org.summarly.summarizing.LexRankRanker;
+import org.summarly.summarizing.RankModifier;
 import org.summarly.summarizing.Ranker;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -21,11 +24,39 @@ public class LexRankSummarizationService implements SummarizationService {
     private Ranker ranker;
     private TextSplitter splitter;
     private Filter filter;
+    private List<RankModifier> rankModifiers;
 
     public LexRankSummarizationService() {
         splitter = new StanfordNLPSplitter();
         ranker = new LexRankRanker();
         filter = new Filter();
+        rankModifiers = Arrays.<RankModifier>asList(text -> text);
+    }
+
+    public List<String> summarise(String s, double ratio) {
+        long start = System.currentTimeMillis();
+        Text text = splitter.split(s, "");
+        if (text.numSentences() < 6) {
+            throw new RuntimeException("The text is too small to apply extractive summary");
+        }
+
+        List<RankedSentence> rankedText = ranker.rank(text);
+        rankedText = modifyRank(rankedText);
+
+        long finish = System.currentTimeMillis();
+        LOGGER.info(String.format(
+                "Processed text of %d sentences in %d ms", text.numSentences(), (finish - start)));
+
+        return filter.filter(rankedText, ratio)
+                .stream().map(p -> p.getSentence().getSentence())
+                .collect(Collectors.<String>toList());
+    }
+
+    private List<RankedSentence> modifyRank(List<RankedSentence> text){
+        for (RankModifier modifier : rankModifiers) {
+            text = modifier.modify(text);
+        }
+        return text;
     }
 
     public Ranker getRanker() {
@@ -42,22 +73,5 @@ public class LexRankSummarizationService implements SummarizationService {
 
     public void setSplitter(TextSplitter splitter) {
         this.splitter = splitter;
-    }
-
-    public List<String> summarise(String s, double ratio) {
-        long start = System.currentTimeMillis();
-        Text text = splitter.split(s, "");
-        if (text.numSentences() < 6) {
-            throw new RuntimeException("The text is too small to apply extractive summary");
-        }
-
-        List<RankedSentence> summary = ranker.rank(text);
-        long finish = System.currentTimeMillis();
-        LOGGER.info(String.format(
-                "Processed text of %d sentences in %d ms", text.numSentences(), (finish - start)));
-
-        return filter.filter(summary, ratio)
-                .stream().map(p -> p.getSentence().getSentence())
-                .collect(Collectors.<String>toList());
     }
 }
