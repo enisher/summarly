9e6df5547642674edf34952450efa2d0aab0dba6
Artem Orobets <enisher@gmail.com>
2014-09-13 18:18:26 +0300

    Scratch splitter impl

---
+++ summarization-lib/src/test/java/org/summarly/segmentation/LuceneSplitterTest.java
@@ -0,0 +1,10 @@
+package org.summarly.segmentation;
+
+import junit.framework.TestCase;
+
+public class LuceneSplitterTest extends TestCase {
+
+    public void testSplit() throws Exception {
+        new LuceneSplitter().split("У попа была собака он ее любил");
+    }
+}
