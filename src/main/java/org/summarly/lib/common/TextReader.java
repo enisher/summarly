81ef1a6bc57206793be38b203b54bae52af4d1c0
Artem Orobets <enisher@gmail.com>
2014-09-14 12:32:39 +0300

    Fix encoding issues

--- src/main/java/org/summarly/lib/common/TextReader.java
+++ src/main/java/org/summarly/lib/common/TextReader.java
@@ -2,14 +2,15 @@ package org.summarly.lib.common;
 
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
+import de.l3s.boilerpipe.sax.HTMLDocument;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.Scanner;
+import java.util.zip.GZIPInputStream;
 
 /**
  * Created by Anton Chernetskij
@@ -50,11 +51,66 @@ public class TextReader {
 
         try {
             ArticleExtractor articleExtractor = ArticleExtractor.getInstance();
-            res = articleExtractor.getText(new URL(url));
+
+            res = articleExtractor.getText(fetch(new URL(url)).toInputSource());
         } catch (IOException | BoilerpipeProcessingException e) {
             throw new RuntimeException(e);
         }
 
         return res;
+    }
+
+    private static HTMLDocument fetch(final URL url) throws IOException {
+        final URLConnection conn = url.openConnection();
+        final String charset = conn.getContentEncoding();
+
+
+        InputStream in = conn.getInputStream();
+
+        final String encoding = conn.getContentEncoding();
+        if(encoding != null) {
+            if("gzip".equalsIgnoreCase(encoding)) {
+                in = new GZIPInputStream(in);
+            } else {
+                System.err.println("WARN: unsupported Content-Encoding: "+encoding);
+            }
+        }
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[4096];
+        int r;
+        while ((r = in.read(buf)) != -1) {
+            bos.write(buf, 0, r);
+        }
+        in.close();
+
+        final byte[] data = bos.toByteArray();
+
+        Charset cs;
+        if (charset != null) {
+            try {
+                cs = Charset.forName(charset);
+            } catch (UnsupportedCharsetException e) {
+                cs = Charset.forName(guessEncoding(data));
+            }
+        } else {
+            cs = Charset.forName(guessEncoding(data));
+        }
+
+        return new HTMLDocument(data, cs);
+    }
+
+    public static String guessEncoding(byte[] bytes) {
+        String DEFAULT_ENCODING = "UTF-8";
+        org.mozilla.universalchardet.UniversalDetector detector =
+                new org.mozilla.universalchardet.UniversalDetector(null);
+        detector.handleData(bytes, 0, bytes.length);
+        detector.dataEnd();
+        String encoding = detector.getDetectedCharset();
+        detector.reset();
+        if (encoding == null) {
+            encoding = DEFAULT_ENCODING;
+        }
+        return encoding;
     }
 }
